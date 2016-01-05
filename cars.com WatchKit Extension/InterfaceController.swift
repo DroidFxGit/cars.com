2f49fadc4b21269765b6f1d387dd6586c6f1ec08
Carlos Vazquez Gomez <c.vazquez@globant.com>
2016-01-05 14:24:26 -0600

    first commit

---
+++ cars.com WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,60 @@
+//
+//  InterfaceController.swift
+//  cars.com WatchKit Extension
+//
+//  Created by DroidFx on 1/5/16.
+//  Copyright © 2016 DroidFx. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+struct rowData {
+    let name: String
+    let imageName: String
+}
+
+
+class InterfaceController: WKInterfaceController {
+
+    @IBOutlet var optionsMenu: WKInterfaceTable!
+    
+    let objects: [rowData] = [
+        rowData(name: "Buy", imageName: "icon_buy"),
+        rowData(name: "Sell & Trade", imageName: "icon_sell"),
+        rowData(name: "Service & Repair", imageName: "icon_service"),
+        rowData(name: "News", imageName: "icon_news")
+    ]
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        setupTable()
+    }
+
+    override func willActivate() {
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        super.didDeactivate()
+    }
+    
+    func setupTable() {
+        
+        optionsMenu.setNumberOfRows(objects.count, withRowType: "OptionMenuRow")
+        
+        for  i in 0 ..< objects.count {
+            let object = objects[i]
+            if let row = optionsMenu.rowControllerAtIndex(i) as? OptionMenuRow {
+                row.name.setText(object.name)
+                row.imageName.setImageNamed(object.imageName)
+            }
+        }
+    }
+    
+    override func table(table: WKInterfaceTable, didSelectRowAtIndex rowIndex: Int) {
+//        self.pushControllerWithName("showDetails", context: countries[rowIndex])
+    }
+
+}
---
+++ cars.com WatchKit Extension/OptionMenuRow.swift
@@ -0,0 +1,17 @@
+//
+//  OptionMenuRow.swift
+//  cars.com
+//
+//  Created by DroidFx on 1/5/16.
+//  Copyright © 2016 DroidFx. All rights reserved.
+//
+
+import Foundation
+import WatchKit
+
+class OptionMenuRow: NSObject {
+    
+    @IBOutlet var imageName: WKInterfaceImage!
+    @IBOutlet var name: WKInterfaceLabel!
+    
+}
